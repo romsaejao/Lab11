4e3064a8dd63978c03d173e1dc7f65e085a132fe
romsaejao <76035596+romsaejao@users.noreply.github.com>
2021-01-15 17:25:26 +0700

    Add files via upload
    11.3 done

---
+++ Untitled11.3cpp.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+#include <string>
+
+using namespace std;
+template <typename T>
+void mySwap(T &,T &);
+int main(){
+	int x, y;
+	string a, b;
+	char p, q;
+	
+	cin >> x >> y >> a >> b >> p >> q;
+	
+	cout << "Before swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	mySwap(x,y);
+	cout << "After swapping:\n";
+	cout << "x = " << x << ", y = " << y << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	mySwap(a,b);
+	cout << "After swapping:\n";
+	cout << "a = " << a << ", b = " << b << "\n";
+	
+	cout << "Before swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+	mySwap(p,q);
+	cout << "After swapping:\n";
+	cout << "p = " << p << ", q = " << q << "\n";
+
+	return 0;
+}
+template <typename T>
+void mySwap(T &x,T &y)
+{
+	T z;
+	z = x;
+	x = y;
+	y = z;
+}
+
+
